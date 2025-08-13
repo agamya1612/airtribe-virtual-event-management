57f4f623605bfe489f310880415f76e719374f32
Agamya <46354525+agamya1612@users.noreply.github.com>
2025-08-13 19:38:59 +0530

    Add files via upload

---
+++ memoryStore.js
@@ -0,0 +1,5 @@
+// data/memoryStore.js
+let users = []; // { id, name, email, passwordHash, role }
+let events = []; // { id, title, date, time, description, organizerId, participants: [] }
+
+module.exports = { users, events };
